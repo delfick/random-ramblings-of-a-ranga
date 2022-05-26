d3bcde534c7d050ca49444e2dbf16ad382409ece
Stephen Moore <stephen@delfick.com>
2022-05-26 16:46:42 +1000

    ci: enable ssr and ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,40 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import viteMainJs from "vite-main-js";
 import preprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import eslint from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const production = process.env.NODE_ENV === "production";
+
+interface ReturnPlugin {
+  (): Plugin;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({ preprocess: preprocess() }), eslint(), tsconfigPaths()],
+  plugins: [
+    (viteMainJs as ReturnPlugin)(),
+    svelte({
+      preprocess: preprocess(),
+      emitCss: true,
+      hot: !production,
+    }),
+    eslint(),
+    tsconfigPaths(),
+  ],
+  build: {
+    polyfillModulePreload: false,
+    cssCodeSplit: false,
+  },
+  optimizeDeps: {
+    exclude: ["@roxi/routify"],
+  },
+  resolve: {
+    dedupe: ["@roxi/routify"],
+  },
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
