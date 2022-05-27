b17cf9e3fe13d5d541203a2546965f83c549d46f
Stephen Moore <stephen@delfick.com>
2022-05-27 23:15:14 +1000

    blog: fixing it teasing us about a code block

--- src/pages/blog/posts/2022-05-27-first.svelte
+++ src/pages/blog/posts/2022-05-27-first.svelte
@@ -1,6 +1,25 @@
+<script>
+  import Prism from "svelte-prism";
+
+  let haveGrammar = false;
+  let gotGrammar = () => {
+    haveGrammar = true;
+  };
+</script>
+
 <!-- routify:options tags=["one", "two"] -->
 <!-- routify:options published="27 May 2022" -->
 <!-- routify:options title="This blog article rules" -->
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/themes/prism-solarizedlight.min.css"
+  />
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/components/prism-python.min.js"
+    on:load={gotGrammar}></script>
+</svelte:head>
 
 <p>👋 Welcome you</p>
 <p>The basic blog page layout is good</p>
@@ -11,3 +30,12 @@
 </blockquote>
 
 <p>Example code block:</p>
+
+<Prism
+  language={haveGrammar ? "python" : undefined}
+  source={`
+    from people import me
+
+    assert me.bestest
+  `}
+/>
