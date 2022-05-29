c21d30b1dbd9e0fd9e3b58798c8a9a641c8a1d4c
Stephen Moore <stephen@delfick.com>
2022-05-29 16:17:30 +1000

    ui: enable dark mode quicker

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,5 +1,10 @@
+<script context="module">
+  import ThemeToggle from "@app/theme_toggle.svelte";
+</script>
+
 <script lang="ts">
   import "./layout.postcss";
 </script>
 
+<ThemeToggle hidden />
 <slot />
