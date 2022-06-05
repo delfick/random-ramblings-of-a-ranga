2eae5ba46210ee59e04e5d5dd36f8a371845ac2e
Stephen Moore <stephen@delfick.com>
2022-06-05 22:38:26 +1000

    resume: fontawesome seems unecessary

--- src/routes/resume/__layout@root.svelte
+++ src/routes/resume/__layout@root.svelte
@@ -13,10 +13,6 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css"
   />
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-  />
 </svelte:head>
 
 <slot />
