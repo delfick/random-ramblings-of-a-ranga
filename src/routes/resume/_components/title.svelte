478416b6acd68ec876b94a0215d19afed295feb3
Stephen Moore <stephen@delfick.com>
2022-07-09 12:27:32 +1000

    resume: sub resume section
    these have titles on the right

--- src/routes/resume/_components/title.svelte
+++ src/routes/resume/_components/title.svelte
@@ -2,13 +2,15 @@
   export let size = 0;
   export let color = "black";
   export let hide_overflow = false;
+  export let right = false;
 
   $: component = `h${size || 1}`;
 </script>
 
 <svelte:element
   this={component}
-  class="title {size === 0 ? '' : 'is-4'}}"
+  class="title {size === 0 ? '' : 'is-4'}"
+  style:text-align={right ? "right" : "auto"}
   style:color
   style:overflow={hide_overflow ? "hidden" : "auto"}
 >
