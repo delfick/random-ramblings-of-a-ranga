85c58aabf15ba61a11f14b2f0674e587cc60f9f8
Stephen Moore <stephen@delfick.com>
2022-07-09 12:35:05 +1000

    resume: break up the notable examples

--- src/routes/resume/engineer.svelte
+++ src/routes/resume/engineer.svelte
@@ -6,6 +6,7 @@
   import { onMount } from "svelte";
   import { dedent } from "ts-dedent";
 
+  import SubResumeSection from "./_components/sub_resume_section.svelte";
   import ResumeSections from "./_components/resume_sections.svelte";
   import ResumeSection from "./_components/resume_section.svelte";
   import SummaryPoints from "./_components/summary_points.svelte";
@@ -127,9 +128,9 @@
     />
     <ResumeSection
       title="Language Skills"
-      title_color="#b019ae"
+      title_color="#1938b0"
       source={dedent`
-    I'm most comfortable with Python, which I've been using for around 15
+    I'm most comfortable with Python, which I've been using for around 16
     years. I am proficient with JavaScript, am comfortable with Typescript and
     have a a passable ability to create web GUIs using React and Redux.
     I built applications for the cloud using Go and gRPC for most of the 5
@@ -152,7 +153,7 @@
     />
     <ResumeSection
       title="Learning Style"
-      title_color="#b019ae"
+      title_color="#1938b0"
       source={dedent`
     I have found that I learn best when I have a need for the skill or
     technology that I'm trying to gain. This means I tend not to dedicate time
@@ -160,7 +161,7 @@
     learn Rust but because I had no need for it I couldn't find the motivation
     to continue that. It did seem interesting though!
 
-    In the Photons framework I mentioned above, it was very clear very early
+    In the Photons framework I mention below, it was very clear very early
     on that using Python async/await would be very useful and this is around
     the time that Python 3.5 just came out. And so over those 5 years I
     created a number of patterns and utilities for working with await/async and
@@ -183,84 +184,99 @@
     `}
     />
     <ResumeSection
-      title="Notable examples: Deployment Tooling"
+      title="Notable examples"
       print_breakable
       title_color="#228c8c"
-      source={dedent`
-    The first job where I provided a significant amount of tooling in the
-    workplace led me to creating a number of deployment tools and then using
-    them to make a consistent deployment pattern for all the applications in
-    my line of business.
+    >
+      <SubResumeSection
+        notoppadding
+        title="Deployment Tooling"
+        source={dedent`
+        A large part of my time at realcommercial.com.au was spent embedded as the
+        single person for deployment support for around 20 engineers across
+        Melbourne and Xi'an. In this time I created three tools in particular: a
+        docker client, a cloudformation management tool, and a general aws
+        synchronisation tool built with boto.
 
-    I made these projects while I was embedded in the realcommercial.com.au
-    team as operations support including a large amount of this time where I
-    was the only person doing operations for around 20 engineers across
-    Melbourne and Xi'an.
+        I used these tools to create a consistent and understandable deployment
+        pattern that let me manage a large workload and enabled my colleagues
+        to focus on what they were building instead of how to get it out the door.
+        `}
+      />
+      <SubResumeSection
+        title="Development system"
+        source={dedent`
+        Years later in my role at Papercut I developed a system that would stand
+        up the development setup for technical and non technical people who added
+        to our systems. This system meant we could make changes to how the
+        development system was setup and after running \`./update.sh\` users
+        of the system would be able to run their local setup with those changes.
 
-    Years later in my role at Papercut I developed a system that would stand up the development
-    setup for technical and non technical people who added to our systems. This
-    system meant we could make changes to how the development system was setup
-    and after running \`./update.sh\` users of the system would be able to run their
-    local setup with those changes.
+        Having the ability to stand up a local system that lets the developer
+        focus on the functionality of the system without being distracted by
+        the operational reality of running code in the cloud and  allows a tight
+        feedback loop on making the code perform it's functional requirements.
 
+        When paired with a staging environment that clones the infrastructure of
+        production, we can can discover and resolve the vast majority of edge
+        cases and bugs with a high level of confidence before code sees production
+        use.
+        `}
+      />
+      <SubResumeSection
+        title="Business wide tooling"
+        source={dedent`
+        When I worked with Smart Light bulbs, my primary focus was developing
+        applications that sit in our GCP kubernetes cluster but my passion led me
+        to creating a large framework of functionality that could interact with
+        devices. This codebase became the defacto second implementation of all
+        the functionality in the consumer mobile apps and factory code. I also
+        built 10 applications on top of that foundation. Code from this project
+        sat in the cloud, was used by support, used in the warehouse, used by
+        sales and used by all engineer teams to interact with our devices during
+        development.
 
-    I have found over my career that having a functional local setup that mirrors
-    functionality shortens the development feedback cycle and is very powerful when
-    combined with a staging environment that mirrors production infrastructure.
-    `}
-    />
-    <ResumeSection
-      title="Notable examples: Business wide tooling"
-      title_color="#228c8c"
-      source={dedent`
-    When I worked with Smart Light bulbs, my primary focus was developing
-    applications that sit in our GCP kubernetes cluster but my passion led me
-    to creating a large framework of functionality that could interact with
-    devices offline for everything they can do. This codebase became the
-    defacto second implementation of all the functionality in the consumer
-    mobile apps and factory code. I also built 10 applications on top of that
-    foundation. Code from this project sat in the cloud, was used by support,
-    used in the warehouse, used by sales and used by all engineer teams to
-    interact with our devices during development. Alongside this in a different
-    code base I have created and maintained a vital part of the factory pipeline.
+        This codebase included over 137 thousand lines of python (including the
+        10 applications and over 60 thousand lines of tests) and was essentially
+        entirely written by me. There's too many features to list here
+        but I'm most proud of my "fake device" which allowed us to emulate the
+        behaviour of different hardware/firmware versions of the product range
+        without having specific hardware or even before firmware changes have
+        been implemented. This code also powered some basic smoke tests that
+        would constantly check that devices could connect to the cloud and be
+        controlled.
+        
+        I find a lot of energy and motivation in these efforts to make it easier
+        for my colleagues (and myself!) to do their jobs. I also have an open source
+        version of this project ([photons.delfick.com](https://photons.delfick.com))
+        that I own and exists on my personal github. I would use this as an
+        unofficial implementation of our LAN protocol when I interacted with our
+        third party developers as I often did on the LIFX community forums and the
+        LIFX subreddit.
 
-    This codebase included over 137 thousand lines of python and was
-    almost entirely being written by me. There's too many features to list here
-    but I'm most proud of my "fake device" which is an implementation of the
-    protocol behaviour. This means we could experiment with protocol changes
-    before being implemented in firmware, as well as spin up any kind of
-    product from the command line without needing that hardware. We also
-    used these to run smoke tests against the LIFX cloud and ensure everything
-    worked correctly.
-    
-    I find a lot of energy and motivation in these efforts to make it easier
-    for my colleagues (and myself!) to do their jobs. I also have an open source
-    version of this project ([photons.delfick.com](https://photons.delfick.com))
-    that I own and exists on my personal github. I would use this as an
-    unofficial implementation of our LAN protocol when I interacted with our
-    third party developers as I often did on the LIFX community forums and the
-    LIFX subreddit.
-    `}
-    />
-    <ResumeSection
-      title="Notable examples: Other projects"
-      title_color="#228c8c"
-      source={dedent`
-    I believe that as a software engineer my job requires a lot more than
-    typing, but the programming part is definitely the most fun and so
-    outside of work I have played with a number of things.
-    
-    Included in these projects are tools for making the process of writing
-    tests easier; data transformation; fast decryption of gpg packets;
-    and a number of older projects that taught me about how to
-    structure large programs. These exist on my Github profile at
-    https://github.com/delfick and I'd love to walk through those and
-    explain their history and functionality.
+        Alongside this in a different code base I created and maintained a vital
+        part of the factory pipeline.
+        `}
+      />
+      <SubResumeSection
+        title="Other projects"
+        source={dedent`
+        I believe that as a software engineer my job requires a lot more than
+        typing, but the programming part is definitely the most fun and so
+        outside of work I have played with a number of things.
+        
+        Included in these projects are tools for making the process of writing
+        tests easier; data transformation; fast decryption of gpg packets;
+        and a number of older projects that taught me about how to
+        structure large programs. These exist on my Github profile at
+        https://github.com/delfick and I'd love to walk through those and
+        explain their history and functionality.
 
-    Since late 2020 I have considerably less time for these side projects but
-    it is something I enjoy to do every so often.
-    `}
-    />
+        Since late 2020 I have considerably less time for these side projects but
+        it is something I enjoy to do every so often.
+        `}
+      />
+    </ResumeSection>
     <Contact
       source={dedent`
     You can find out more about me on my linkedIn profile at
