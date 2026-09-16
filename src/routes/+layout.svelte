b1065c82564f4b8a661e15c08ccc831ab878ab0d
Stephen Moore <stephen.moore@kraken.tech>
2026-09-16 17:01:42 +1000

    Add bluesky rel=me link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,7 +21,10 @@
   <link rel="apple-touch-icon" sizes="180x180" href={AppleTouchIcon} />
 
   <link rel="manifest" href="/site.webmanifest" />
-
+  <link
+    href="https://bsky.brid.gy/ap/did:plc:myln4meshy6cfnpyky6sz7kb"
+    rel="me atproto"
+  />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="theme-color" content="#ffffff" />
 </svelte:head>
